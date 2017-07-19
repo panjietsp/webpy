48307e5859c4bb9265c1e50c10a3621373060e73
pan <jie.pan@telecom-sudparis.eu>
2017-07-19 16:48:54 +0200

    ddd

--- code.py
+++ code.py
@@ -1,12 +1,33 @@
 import web
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/wxvalid', 'wx_validation'
 )
 
 class index:
     def GET(self):
-        return "Hello, welcome to PAN's webpy"
+        return "Hello, welcome to PAN's webpy(vf2)"
+
+class wx_validation:
+    def GET(self):
+        data=web.input()
+	signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        token = 'wangyuweibaobei'
+        echostr = data.echostr
+
+	list = [token,timestamp,nonce]
+	list.sort()
+	list2 = ''.join(list)
+	sha1 = hashlib.sha1()
+	sha1.update(list2.encode('utf-8'))
+	hashcode = sha1.hexdigest()
+
+	if hashcode == signature:
+		return echostr
+
 
 app = web.application(urls, globals())
 application = app.wsgifunc()
