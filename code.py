b5fe93ee1be4bab7eccfbcadc40e4bab38d6ff0a
pan <jie.pan@telecom-sudparis.eu>
2017-07-19 16:33:08 +0200

    ddd

--- code.py
+++ code.py
@@ -8,7 +8,6 @@ class index:
     def GET(self):
         return "Hello, welcome to PAN's webpy"
 
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    application = app.wsgifunc()
+app = web.application(urls, globals())
+application = app.wsgifunc()
     #app.run()
